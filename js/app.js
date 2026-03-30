7e45009a283b8b8f59eeb8f8024a4755a0b29e15
Colin Downie <91563252+ColinDownie@users.noreply.github.com>
2026-03-30 12:21:05 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Mobile nav toggle
+document.addEventListener("DOMContentLoaded", function () {
+  var toggle = document.querySelector(".nav-toggle");
+  var navList = document.querySelector("nav ul");
+
+  if (toggle && navList) {
+    toggle.addEventListener("click", function () {
+      navList.classList.toggle("nav-open");
+    });
+  }
+});
